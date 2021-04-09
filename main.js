a2cac5c3f187d2a3b92f2157bd9e007f8710fd32
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-04-09 14:04:36 +0100

    Add eventlistener

---
+++ main.js
@@ -0,0 +1,11 @@
+const toActiveModal = document.querySelector('.to_active_modal');
+const modalContainer = document.querySelector('.contact_modal_container');
+const modalCloser = document.querySelector('.close_modal');
+
+toActiveModal.addEventListener("click", () => {
+  modalContainer.classList.add('active_modal');
+});
+
+modalCloser.addEventListener("click", () => {
+  modalContainer.classList.remove('active_modal');
+});
