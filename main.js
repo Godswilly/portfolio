7b95fa0414f783a8169cf983c8e886989f5c949c
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-04-09 16:35:33 +0100

    Add code linters

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const toActiveModal = document.querySelector('.to_active_modal');
 const modalContainer = document.querySelector('.contact_modal_container');
 const modalCloser = document.querySelector('.close_modal');
 
-toActiveModal.addEventListener("click", () => {
+toActiveModal.addEventListener('click', () => {
   modalContainer.classList.add('active_modal');
 });
 
-modalCloser.addEventListener("click", () => {
+modalCloser.addEventListener('click', () => {
   modalContainer.classList.remove('active_modal');
 });
